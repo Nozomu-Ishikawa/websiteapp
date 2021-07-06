7eba67ae4352057fb3fb121058ca479fccf8c4fa
Nozomu-Ishikawa <koihime0musou@gmail.com>
2021-07-06 15:52:54 +0900

    メイン画面　仮完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  layout 'layouts/user_profile_layout', only: [:show]
+
+  before_action :set_command, only: :index
+  # before_action :costomer_params
+
+  def index
+    @user = User.all
+  end
+
+  def show
+  end
+
+  # def edit
+  # end
+
+  private
+
+  # def customer_params
+  #   params.require(:user).permit().merge(user_id: current_user.id)
+  # end
+
+  def set_command
+    @user = current_user
+  end
+
 end
