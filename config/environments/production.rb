188560820602ea6770812f35185a5b63a19acf3f
Nozomu-Ishikawa <koihime0musou@gmail.com>
2021-07-23 14:31:47 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = {host: 'https://game-reviewers.herokuapp.com/' }
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDGRID_ACCOUNT'],
-      :password => ENV['SEDGRID_ID'],
-      :domain => "heroku.com",
-      :address => "smtp.sendgrid.net",
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
+  # config.action_mailer.default_url_options = {host: 'https://game-reviewers.herokuapp.com/' }
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #     :user_name => ENV['SENDGRID_ACCOUNT'],
+  #     :password => ENV['SEDGRID_ID'],
+  #     :domain => "heroku.com",
+  #     :address => "smtp.sendgrid.net",
+  #     :port => 587,
+  #     :authentication => :plain,
+  #     :enable_starttls_auto => true
+  #   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
