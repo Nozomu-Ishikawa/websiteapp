393b4aa5c3e78d622ddd47f6d27e0847e9821ff9
Nozomu-Ishikawa <koihime0musou@gmail.com>
2021-07-25 16:35:58 +0900

    メール認証

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => Rails.application.credentials.gmail[:user_name],
-      :password => Rails.application.credentials.gmail[:password],
+      :user_name => ENV['GMAIL_USERNAME'],
+      :password => ENV['GMAIL_PASSWORD'],
       :domain => 'gmail.com',
       :address => "smtp.gmail.com",
       :port => Settings.smtp[:port],
