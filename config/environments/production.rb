8548adf9ef58a6205f952dc6e10e71a54457fe34
Nozomu-Ishikawa <koihime0musou@gmail.com>
2021-07-25 15:18:40 +0900

    メール認証

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.default_url_options = {  :host =>  'https://game-reviewers.herokuapp.com/' }
+  config.action_mailer.default_url_options = {  host: 'https://game-reviewers.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
