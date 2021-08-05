5149cf67bdf7df28464b026e473fb2e68fe99468
Nozomu-Ishikawa <koihime0musou@gmail.com>
2021-08-05 17:05:44 +0900

    お問い合わせフォーム

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   scope module: :users do
     resources :notices, only: %i[index]
     resources :articles, only: %i[index show]
+
+    resources :opinions, only: %i[create]
   end
 
   resources :reviews, except: %i[new] do
